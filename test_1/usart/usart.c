a581a642e7043fbc3799ae1011f6b8a4bc8babaa
m <3457532883@qq.com>
2024-10-17 12:53:00 +0800

    main

---
+++ test_1/usart/usart.c
@@ -0,0 +1,24 @@
+#include <pico/stdlib.h>
+#include <hardware/uart.h>
+
+int main()
+{
+
+    stdio_init_all();
+
+    uart_init(uart0, 115200);
+    
+    gpio_function(1, GPIO_FUNC_UART);
+    gpio_function(0, GPIO_FUNC_UART);   
+
+    uart_set_format(uart0, 8, 1, UART_PARITY_NONE);
+
+    while(1)
+    {
+        uart_puts(uart0, "Hello, world!\n");
+        sleep_ms(1000);
+    }
+
+    return 0;
+
+}
